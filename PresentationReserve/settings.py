427e3d9b535fce379c5ec9395ac5d8da60f858a1
kianbehzad <kian.behzad@gmail.com>
2018-11-26 22:07:44 +0330

    creating reserve mainpage interface

--- PresentationReserve/settings.py
+++ PresentationReserve/settings.py
@@ -123,5 +123,7 @@ STATIC_URL = '/static/'
 # see https://help.pythonanywhere.com/pages/DjangoStaticFiles for more info
 MEDIA_ROOT = u'/home/kianbehzad/PresentationReserve/media'
 MEDIA_URL = '/media/'
-STATIC_ROOT = u'/home/kianbehzad/PresentationReserve/static'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 STATIC_URL = '/static/'
--- reserve/views.py
+++ reserve/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def reserve_mainpage(request):
-    return HttpResponse("reserved")
+    template = loader.get_template("reserve_mainpage.html")
+    context = {}
+    return HttpResponse(template.render(context, request))
 
 
