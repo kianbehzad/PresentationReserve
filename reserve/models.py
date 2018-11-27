ca919028fcaa2e89677e7b166ce8a46109b9b46c
kianbehzad <kian.behzad@gmail.com>
2018-11-27 20:06:45 +0330

    create Student model in reserve App

--- reserve/models.py
+++ reserve/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from myadmin.models import Datetime
 
 # Create your models here.
+
+class Student(models.Model):
+    stdnum = models.IntegerField()
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    datetime = models.ForeignKey(Datetime, related_name="datetime_students", null=True, on_delete=models.SET_NULL, blank=True)
+    topic = models.CharField(max_length=100)
+    is_verified = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return str(self.stdnum) + ' - ' + self.email + ' - ' + self.datetime.__str__()
