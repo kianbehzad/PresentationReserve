fd0e36f3cca09c2a1f7b54dc2e16a3fb8256a695
kianbehzad <kian.behzad@gmail.com>
2018-11-27 18:46:31 +0330

    making Datetime Model in myadmin App

--- myadmin/admin.py
+++ myadmin/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Datetime
 
 # Register your models here.
+
+admin.site.register(Datetime)
--- myadmin/models.py
+++ myadmin/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Datetime(models.Model):
+    date = models.CharField(max_length=15)
+    num_stars = models.IntegerField()
+
+    def __str__(self):
+        return self.date + '  ' + self.num_stars + 'X*'
