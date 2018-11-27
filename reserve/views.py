d5b6d154ae1d8adf53bba38e8f35b44ef49862bb
kianbehzad <kian.behzad@gmail.com>
2018-11-27 19:43:59 +0330

    fix choice of datetimes in reserve_template.html

--- reserve/views.py
+++ reserve/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from myadmin.models import Datetime
+
 
 # Create your views here.
 
 def reserve_mainpage(request):
-    template = loader.get_template("reserve_mainpage.html")
-    context = {}
-    return HttpResponse(template.render(context, request))
+    stdnum = request.GET.get("name")
+    name = request.GET.get("name1")
+    email = request.GET.get("email")
+    date = request.GET.get("service")
+    topic = request.GET.get("message")
+    if stdnum == None:
+        template = loader.get_template("reserve_mainpage.html")
+        all_Datetime = Datetime.objects.all()
+        context = {'all_Datetime': all_Datetime}
+        return HttpResponse(template.render(context, request))
+    #else
+    return HttpResponse("salam")
+
 
 
