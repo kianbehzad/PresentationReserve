cf50630732be9866b681c0c38739d11ae0570d6f
kianbehzad <kian.behzad@gmail.com>
2018-12-03 22:57:45 +0330

    make list.xlsx much prettier

--- myadmin/views.py
+++ myadmin/views.py
@@ -5,11 +5,56 @@ from django.http import HttpResponse
 from PresentationReserve.settings import BASE_DIR
 import os
 import openpyxl
+from openpyxl.styles import Border, Side, PatternFill, Font, GradientFill, Alignment
 
 # Create your views here.
 
 def sort_datatime(elem):
     return elem.datetime.year*365 + elem.datetime.month*30 + elem.datetime.day
+
+def as_text(value):
+    if value is None:
+        return ""
+    return str(value) + '   '
+
+def style_range(ws, cell_range, border=Border(), fill=None, font=None, alignment=None):
+    """
+    Apply styles to a range of cells as if they were a single cell.
+
+    :param ws:  Excel worksheet instance
+    :param range: An excel range to style (e.g. A1:F20)
+    :param border: An openpyxl Border
+    :param fill: An openpyxl PatternFill or GradientFill
+    :param font: An openpyxl Font object
+    """
+
+    top = Border(top=border.top)
+    left = Border(left=border.left)
+    right = Border(right=border.right)
+    bottom = Border(bottom=border.bottom)
+
+    first_cell = ws[cell_range.split(":")[0]]
+    if alignment:
+        ws.merge_cells(cell_range)
+        first_cell.alignment = alignment
+
+    rows = ws[cell_range]
+    if font:
+        first_cell.font = font
+
+    for cell in rows[0]:
+        cell.border = cell.border + top
+    for cell in rows[-1]:
+        cell.border = cell.border + bottom
+
+    for row in rows:
+        l = row[0]
+        r = row[-1]
+        l.border = l.border + left
+        r.border = r.border + right
+        if fill:
+            for c in row:
+                c.fill = fill
 
 def list(request):
     # create the list
@@ -28,8 +73,18 @@ def list(request):
     sheet['E1'] = 'TOPIC'
     sheet['F1'] = 'DATE TIME'
     num = 1
+    thin = Side(border_style="thin", color="ffffff")
+    double = Side(border_style="double", color="ffffff")
+    border = Border(top=double, left=thin, right=thin, bottom=double)
+    fill = GradientFill(stop=("000000", "000000"))
+    font = Font(b=True, color="ffffff")
+    al = Alignment(horizontal="center", vertical="center")
+    last_datetime = ''
     for i in range(len(student_list)):
-        if i >= 1 and student_list[i].datetime != student_list[i-1].datetime:
+        if student_list[i].datetime != last_datetime:
+            sheet['A' + str(cnt)] = student_list[i].datetime.__str__()
+            style_range(sheet, 'A'+str(cnt)+':F'+str(cnt), border=border, fill=fill, font=font, alignment=al)
+            cnt += 1
             num = 1
         sheet['A' + str(cnt)] = num
         sheet['B' + str(cnt)] = student_list[i].email
@@ -37,8 +92,16 @@ def list(request):
         sheet['D' + str(cnt)] = student_list[i].name
         sheet['E' + str(cnt)] = student_list[i].topic
         sheet['F' + str(cnt)] = student_list[i].datetime.__str__()
+        last_datetime = student_list[i].datetime
         num += 1
         cnt += 1
+    for col in sheet.columns:
+        for cell in col:
+            cell.alignment = al
+    for column_cells in sheet.columns:
+        length = max(len(as_text(cell.value)) for cell in column_cells)
+        sheet.column_dimensions[column_cells[0].column].width = length
+
     os.chdir(os.path.join(BASE_DIR, "media"))
     if os.path.exists('list.xlsx'):
         os.remove('list.xlsx')
